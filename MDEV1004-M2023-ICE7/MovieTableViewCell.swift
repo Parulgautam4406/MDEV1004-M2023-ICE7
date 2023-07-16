5fe02052d1ffd68a67c6ece8e112d3a0ce0c4b57
gauravgeorgian <200520343@student.georgianc.on.ca>
2023-07-17 04:09:01 +0530

    FILES ADDED

---
+++ MDEV1004-M2023-ICE7/MovieTableViewCell.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class MovieTableViewCell: UITableViewCell
+{
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var studioLabel: UILabel!
+    
+    @IBOutlet weak var ratingLabel: UILabel!
+}
